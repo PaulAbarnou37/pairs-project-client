4a127b17b3cde26a58ff90ed654d7d6ac9c74601
Paul Abarnou <p.abarnou37@gmail.com>
2018-10-01 19:00:11 +0200

    added calendar style & restrictions

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -3,6 +3,7 @@ import Moment from 'moment';
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
+
 class ResultPage extends React.Component {
   constructor(props) {
     super(props);
@@ -15,16 +16,36 @@ class ResultPage extends React.Component {
 
   render() { 
     const { oneSearch } = this.props.location.state;
-    const birthdayStyle = `.DayPicker-Day--highlighted {
-      background-color: orange;
+    const mySelectedDays = `.DayPicker-Day--mySelectedDays {
+      background-color: green;
+      color: white;
+    }`;
+    const searchSelectedDays = `.DayPicker-Day--searchSelectedDays {
+      background-color: blue;
+      color: white;
+    }`;
+    const duplicatedDays = `.DayPicker-Day--duplicatedDays {
+      background-color: red;
       color: white;
     }`;
     
     const modifiers = {
-      highlighted: [],
+      mySelectedDays: [],
+      searchSelectedDays: [],
+      duplicatedDays: [],
     };
 
-    // oneSearch.mySelectedDaysInRange.forEach( )
+    oneSearch.mySelectedDaysInRange.forEach(oneDay => {
+      return modifiers.mySelectedDays.push(new Date(oneDay));
+    })
+
+    oneSearch.searchesSelectedDaysInRange.forEach(oneDay => {
+      return modifiers.searchSelectedDays.push(new Date(oneDay));
+    })
+
+    oneSearch.duplicatedDays.forEach(oneDay => {
+      return modifiers.duplicatedDays.push(new Date(oneDay));
+    })
     
     console.log(oneSearch);
 
@@ -35,8 +56,8 @@ class ResultPage extends React.Component {
         <p>He stays from <br/>{Moment(oneSearch.searchObject.startDate).format('DD/MM/YYYY')} to {Moment(oneSearch.searchObject.endDate).format('DD/MM/YYYY')}</p>
         <p>You stay from <br/>{Moment(oneSearch.mySearch.startDate).format('DD/MM/YYYY')} to {Moment(oneSearch.mySearch.endDate).format('DD/MM/YYYY')}</p>
         <p>Date range in common: <br/> {Moment(oneSearch.dateRangeMatch.dateRangeIntersection.start).format('DD/MM/YYYY')} - {Moment(oneSearch.dateRangeMatch.dateRangeIntersection.start).format('DD/MM/YYYY')}</p>
-        <style>{birthdayStyle}</style>
-        <DayPicker modifiers={modifiers} />
+        <style>{mySelectedDays}{searchSelectedDays}{duplicatedDays}</style>
+        <DayPicker modifiers={modifiers} month={new Date(oneSearch.dateRangeMatch.dateRangeIntersection.start)} fromMonth={new Date(oneSearch.dateRangeMatch.dateRangeIntersection.start)} toMonth={new Date(oneSearch.dateRangeMatch.dateRangeIntersection.end)} />
       </div>
       );
   }
